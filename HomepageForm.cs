f0ad00df020319a63eef9ce8a3262a110a02e5c0
MacQuy <macquydev77@gmail.com>
2025-04-06 12:34:27 +0700

    Add many forms

--- HomepageForm.cs
+++ HomepageForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace NT106
 {
-    public partial class Form1 : Form
+    public partial class HomepageForm : Form
     {
-        public Form1()
+        public HomepageForm()
         {
             InitializeComponent();
         }
