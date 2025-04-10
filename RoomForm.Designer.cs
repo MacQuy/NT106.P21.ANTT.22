f2154e306a105341e4f0aca00477f10de8811fc4
MacQuy <macquydev77@gmail.com>
2025-04-10 17:30:10 +0700

    Design Room form

--- RoomForm.Designer.cs
+++ RoomForm.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Title = new System.Windows.Forms.Label();
+            this.ChessIcon = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.ChessIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Monotype Corsiva", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Title.Location = new System.Drawing.Point(11, 9);
+            this.Title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(145, 45);
+            this.Title.TabIndex = 13;
+            this.Title.Text = "Chess AI";
+            // 
+            // ChessIcon
+            // 
+            this.ChessIcon.Image = global::NT106.Properties.Resources.ChessIcon;
+            this.ChessIcon.ImageRotate = 0F;
+            this.ChessIcon.Location = new System.Drawing.Point(160, 9);
+            this.ChessIcon.Margin = new System.Windows.Forms.Padding(2);
+            this.ChessIcon.Name = "ChessIcon";
+            this.ChessIcon.Size = new System.Drawing.Size(38, 45);
+            this.ChessIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ChessIcon.TabIndex = 14;
+            this.ChessIcon.TabStop = false;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(67, 98);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(119, 41);
+            this.guna2HtmlLabel1.TabIndex = 79;
+            this.guna2HtmlLabel1.Text = "Rooms";
+            // 
+            // RoomForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1166, 718);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.ChessIcon);
+            this.Controls.Add(this.Title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RoomForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RoomForm";
+            ((System.ComponentModel.ISupportInitialize)(this.ChessIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label Title;
+        private Guna.UI2.WinForms.Guna2PictureBox ChessIcon;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
     }
 }
